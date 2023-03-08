0ad16d41955e9aea3d466dfd34bd8fcf642b8d97
playerandplayer <oscar.dk.roskilde@gmail.com>
2023-03-08 17:35:43 +0100

    office movement or smth idk

---
+++ Assets/scripts/OfficeControl.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OfficeControl : MonoBehaviour
+{
+    public float rotateSpeed = 5f;
+
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        if (horizontalInput != 0)
+        {
+            transform.Rotate(Vector3.up, horizontalInput * rotateSpeed);
+        }
+    }
+}
+
